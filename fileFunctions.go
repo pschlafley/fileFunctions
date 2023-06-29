169b77e414ac7c9875107292e94e1db6e2523288
Peyton Schlafley <pschlafley0@gmail.com>
2023-06-29 13:41:07 -0400

    updated the creat file func

--- fileFunctions.go
+++ fileFunctions.go
@@ -9,10 +9,10 @@ func CreateFile(fileName, path string) {
 	if _, pathErr := os.Stat(path); pathErr != nil {
 		if os.IsNotExist(pathErr) {
 			fmt.Print(pathErr)
-			os.Mkdir("examples", 0700)
+			os.Mkdir(path, 0700)
 		} else {
 			fmt.Print(pathErr)
-			os.Chmod("examples", 0700)
+			os.Chmod(path, 0700)
 		}
 	}
 
