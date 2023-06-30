3ed9f519357d8fa701e2783c4b88c9d3ba38ecd2
Peyton Schlafley <pschlafley0@gmail.com>
2023-06-29 20:02:13 -0400

    added writeFile function and updated the error message to describe that the folder is being created in the createFile function

--- fileFunctions.go
+++ fileFunctions.go
@@ -2,14 +2,25 @@ package fileFunctions
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
+
+func writeFile(fileName, path string) {
+	files, err := os.ReadDir(path)
+
+	if files != nil {
+		CreateFile(fileName, path)
+	} else if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func CreateFile(fileName, path string) {
 	if _, pathErr := os.Stat(path); pathErr != nil {
 		if os.IsNotExist(pathErr) {
 			os.Mkdir(path, 0700)
-			fmt.Print(pathErr)
+			fmt.Printf("Error: %s\n Creating it now...", pathErr)
 		} else {
 			os.Chmod(path, 0700)
 			fmt.Print(pathErr)
