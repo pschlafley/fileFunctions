71a3ff71e3677c236ac5241746eb3d2ad1777a78
Peyton Schlafley <pschlafley0@gmail.com>
2023-07-06 22:26:31 -0400

    changed the library to a package so that I can test locally and not have to push to github and test on the go_terminal app everytime

--- fileFunctions.go
+++ fileFunctions.go
@@ -1,4 +1,4 @@
-package fileFunctions
+package main
 
 import (
 	"fmt"
@@ -7,22 +7,69 @@ import (
 	"os"
 )
 
-func FindFile(fileName, path string) {
+func main() {
+	FindFile2("text.txt", ".")
+}
+
+func FindFile2(fileName, path string) {
 	//root := "/Users/peyton.schlafley/Code/go-repos/go_terminal"
 	var fileSystem fs.FS = os.DirFS(path)
+
+	var data []fs.DirEntry
 
 	fs.WalkDir(fileSystem, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(path)
+
+		data = append(data, d)
+
+		return nil
+	})
+
+	for i := 0; i < len(data); i++ {
+		if data[i].Name() == fileName {
+			fmt.Printf("fileName: %v \n data[i]: %v", fileName, data[i].Name())
+		} else if data[i].Name() != fileName {
+			fmt.Printf("fileName not found")
+		}
+	}
+}
+
+func FindFile(fileName, path string) (string, string) {
+	//root := "/Users/peyton.schlafley/Code/go-repos/go_terminal"
+	var fileSystem fs.FS = os.DirFS(path)
+
+	var data []fs.DirEntry
+
+	var fileWasFound bool
+
+	fs.WalkDir(fileSystem, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		data = append(data, d)
 
 		if d.Name() == fileName {
 			fmt.Printf("Found the file named: %v \n Path: %v", fileName, path)
+			fileWasFound = true
+		} else {
+			fmt.Printf("The file: %v was not found in this path: %v", fileName, path)
+			fileWasFound = false
 		}
 
 		return nil
 	})
+
+	print(data)
+
+	if fileWasFound {
+		return fileName, path
+	} else {
+		return fmt.Sprintf("FileName: %v was not found", fileName), ""
+	}
+
 }
 
 func EditFile(fileName, path string) {
